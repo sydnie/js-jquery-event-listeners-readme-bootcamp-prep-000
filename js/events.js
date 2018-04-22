f3490ca4e7e1d52c2e2f216d3a0e8a825e029309
Sydnie T <sydnietom@gmail.com>
2018-04-22 04:23:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,6 +11,12 @@ function frameIt() {
   });
 }
 
+function submitIt() {
+  $('event').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
